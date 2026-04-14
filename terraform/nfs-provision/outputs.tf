ab4481e0400221b9232d4272ad368f5ae94f8c00
Mahatva Garg <mg26471@netapp.com>
2026-04-14 15:11:43 +0530

    feat: update NFS provision Terraform configuration (#9)

--- terraform/nfs-provision/outputs.tf
+++ terraform/nfs-provision/outputs.tf
@@ -1,16 +1,11 @@
 output "volume_name" {
   description = "Name of the created volume"
-  value       = netapp-ontap_storage_volume.nfs_vol.name
+  value       = netapp-ontap_volume.nfs_vol.name
 }
 
 output "mount_path" {
   description = "NAS junction path for mounting"
-  value       = netapp-ontap_storage_volume.nfs_vol.nas.path
-}
-
-output "export_policy" {
-  description = "Name of the export policy assigned to the volume"
-  value       = netapp-ontap_protocols_nfs_export_policy.vol_policy.name
+  value       = netapp-ontap_volume.nfs_vol.nas.junction_path
 }
 
 output "client_match" {
